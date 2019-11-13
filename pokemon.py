7c97d51e9fc360377e981161aaac81411f4d334d
BLanterman <brysonlanterman@gmail.com>
2019-11-12 16:43:04 -0800

    Added pokemon.py initital class definition

---
+++ pokemon.py
@@ -0,0 +1,8 @@
+class Pokemon:
+    def __init__(self, name, level, element_type, maximum_health, current_health, knocked_out):
+        self.name = name
+        self.level = level
+        self.element_type = element_type
+        self.maximum_health = maximum_health
+        self.current_health = current_health
+        self.knocked_out = knocked_out
