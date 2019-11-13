cb7bb4ebfca25b0553c1ec9d4ba132174f2ed167
BLanterman <brysonlanterman@gmail.com>
2019-11-12 16:55:29 -0800

    Added repr definition and class test code

--- pokemon.py
+++ pokemon.py
@@ -6,3 +6,15 @@ class Pokemon:
         self.maximum_health = maximum_health
         self.current_health = current_health
         self.knocked_out = knocked_out
+    def __repr__(self):
+        return self.name
+
+test_name = "Pikachu"
+test_level = 1
+test_element_type = "Electric"
+test_maximum_health = 30
+test_current_health = 25
+test_knocked_out = False
+
+pikachu = Pokemon(test_name, test_level, test_element_type, test_maximum_health, test_current_health, test_knocked_out)
+print(pikachu)
